0421bbc0e5777669aea68afc991ab0c9cc3a6425
dasha <dasha.motry2002@gmail.ru>
2024-06-29 23:18:22 +0300

    done

---
+++ src/Book.java
@@ -0,0 +1,28 @@
+public class Book {
+    public String title;
+    public Author author;
+    public int year;
+
+    public Book(String title, Author author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+}
